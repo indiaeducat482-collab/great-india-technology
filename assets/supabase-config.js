15a47b37312cb3125a8ee6730f975a906bf92f2d
indiaeducat482-collab <indiaeducat482@gmail.com>
2026-09-05 17:17:26 +0530

    Delete assets directory

--- assets/supabase-config.js
+++
@@ -1,3 +0,0 @@
-window.GIT_SUPABASE_URL = 'https://dfjmflwavidxfmngrwno.supabase.co';
-// Paste your Supabase Publishable Key below. Never use a service_role/secret key in frontend code.
-window.GIT_SUPABASE_KEY = 'PASTE_YOUR_SUPABASE_PUBLISHABLE_KEY_HERE';
